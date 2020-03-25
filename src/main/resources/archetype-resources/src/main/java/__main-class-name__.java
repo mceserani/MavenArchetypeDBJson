e96696e57f66a2c7e488699d7ae8361168b5807c
Matteo Ceserani <ceserani@gmail.com>
2020-03-25 15:56:19 +0100

    First commit

---
+++ src/main/resources/archetype-resources/src/main/java/__main-class-name__.java
@@ -0,0 +1,18 @@
+package ${package};
+
+public class ${main-class-name} {
+	
+	private ${main-class-name}(){
+	}
+
+	/**
+	 * 
+	 * @param args Command line arguments
+	 * @author 
+	 * @version 
+	 */
+    public static void main(String[] args){
+       
+    }
+
+}
---
+++ src/main/resources/archetype-resources/src/test/java/__main-class-name__Test.java
@@ -0,0 +1,20 @@
+package ${package};
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for ${main-class-name}.
+ */
+class ${main-class-name}Test {
+
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void test${main-class-name}() {
+        assertEquals(1, 1);
+    }
+
+}
